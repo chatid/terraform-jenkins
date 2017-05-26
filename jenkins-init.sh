d9c1f3dee853af85292d737a2e2a0956f309fd08
Kevin Calman <kevin.calman@welcomecommerce.com>
2017-05-26 10:44:02 -0500

    DEVO-65 Inital MVP

--- jenkins-init.sh
+++ jenkins-init.sh
@@ -6,6 +6,6 @@ JENKINS_HOME=/var/lib/jenkins
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo $SHELL -c "yes '' | apt-get upgrade -y"
+#sudo $SHELL -c "yes '' | apt-get upgrade -y"
 sudo apt-get install -y jenkins
 
